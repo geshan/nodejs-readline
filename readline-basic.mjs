cc19dee973fea571d960b18cdae7ebda8c904235
Geshan Manandhar <geshan@gmail.com>
2022-10-10 21:42:55 +1100

    Revert "Adds chalk and styled output on readline-basic.mjs"

--- readline-basic.mjs
+++ readline-basic.mjs
@@ -1,17 +1,12 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import chalk from 'chalk';
 
 const rl = readline.createInterface({ input, output });
 try {
   const answer = await rl.question('What is 4x4 equals? ');
 
-  const correct = chalk.green.bgGray.underline.bold;
-  const incorrect = chalk.red.bgWhite.bold;
-  const isCorrect = answer.trim() === '16';
-  const output = isCorrect ? correct(`${answer.trim()} is correct!`) : incorrect(`${answer.trim()} is incorrect. Try again.`);
-  
-  console.log(output);
+  const correctOrNot = answer.trim() === '16' ? 'correct!' : 'incorrect. Try again.';
+  console.log(`${answer.trim()} is ${correctOrNot}`);
 } catch(err) {
   console.log(`Error: `, err);
 } finally {
